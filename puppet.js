63e6e050616424e61b1710d87a40c2edc5677f11
kanishkthapa <kanishk@betalectic.com>
2024-06-19 12:37:40 +0530

    one final template

--- puppet.js
+++ puppet.js
@@ -5,71 +5,11 @@ const handlebars = require("handlebars");
 const path = require("path");
 // const mime = require("mime");
 
-<<<<<<< HEAD
-//const templateSource = fs.readFileSync("ALSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("AKSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("AZSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("ARSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("CASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("COSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("CTSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("DCSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("FLSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("GASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("HISurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("IDSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("ILSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("INSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("IASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("KSSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("KYSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("LASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MESurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MDSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MISurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MNSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MSSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MOSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("MTSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NESurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NVSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NHSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NJSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NMSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NYSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NCSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("NDSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("OHSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("OKSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("ORSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("PASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("RISurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("SCSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("SDSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("TNSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("TXSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("UTSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("VTSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("VASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("WASurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("WVSurplusLinesNotice.hbs", "utf8");
-//const templateSource = fs.readFileSync("WISurplusLinesNotice.hbs", "utf8");
-const templateSource = fs.readFileSync("WYSurplusLinesNotice.hbs", "utf8");
-
-// const templateSource = fs.readFileSync("paramountCocIssue.hbs", "utf8");
-//const templateSource = fs.readFileSync("onceCoverageDocument.hbs", "utf8");
-//const templateSource = fs.readFileSync("onceCompleteTemplate.hbs", "utf-8");
+const templateSource = fs.readFileSync("onceFinalTemplate.hbs", "utf-8");
+// const templateSource = fs.readFileSync("onceCompleteTemplate.hbs", "utf-8");
 // const templateSource = fs.readFileSync("test1.hbs", "utf-8");
 // const templateSource = fs.readFileSync("paramountCocIssue.hbs", "utf8");
 // const templateSource = fs.readFileSync("onceCoverageDocument.hbs", "utf8");
-
-=======
-const templateSource = fs.readFileSync("onceCompleteTemplate.hbs", "utf-8");
-// const templateSource = fs.readFileSync("test1.hbs", "utf-8");
-// const templateSource = fs.readFileSync("paramountCocIssue.hbs", "utf8");
-// const templateSource = fs.readFileSync("onceCoverageDocument.hbs", "utf8");
->>>>>>> c36de38e0f40cb2cb44d22305905dcf641813763
 // const templateSource = fs.readFileSync("oncePolicyTemplate.hbs", "utf8");
 // const templateSource = fs.readFileSync("oncePolicyEmail.hbs", "utf8");
 //const templateSource = fs.readFileSync("onceServiceSuiteTemplate.hbs", "utf8");
@@ -99,6 +39,17 @@ function getImageBase64(filePath) {
 }
 
 app.get("/document", async (req, res) => {
+  handlebars.registerHelper("switch", function (value, options) {
+    this.switch_value = value;
+    return options.fn(this);
+  });
+
+  handlebars.registerHelper("case", function (value, options) {
+    if (value == this.switch_value) {
+      return options.fn(this);
+    }
+  });
+
   const template = handlebars.compile(templateSource);
   const imagePath = path.join(__dirname, "images", "once-logo.png");
 
@@ -117,6 +68,7 @@ app.get("/document", async (req, res) => {
     premium: "$ 200",
     deductible: "$ 0",
     imageBase64: getImageBase64(imagePath),
+    state: "AK",
   };
 
   const html = template(data);
